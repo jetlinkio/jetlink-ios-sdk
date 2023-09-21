85cf7e8c653ec096e8b254056ca8d879a4a2ad83
Jetlink <kaan@jetlink.io>
2023-09-21 11:38:09 +0300

    Delete Jetlink.Example.Objective-C directory

--- Jetlink.Example.Objective-C/Pods/Target Support Files/Pods-JetlinkSample/Pods-JetlinkSample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_JetlinkSample : NSObject
-@end
-@implementation PodsDummy_Pods_JetlinkSample
-@end
--- Jetlink.Example.Objective-C/Pods/Target Support Files/Pods-JetlinkSampleUITests/Pods-JetlinkSampleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_JetlinkSampleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_JetlinkSampleUITests
-@end
